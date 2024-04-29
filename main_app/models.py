7028b6e000d582b6e5c855f84b2df64253f930f3
Andrea Macias <andreamacrz@gmail.com>
2024-04-28 22:53:21 -0400

    login/logout and NYT API set up

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+CATEGORY = (
+    ('G', 'General'),
+    ('S', 'Science'),
+    ('C', 'Culture'),
+    ('G', 'Geopolitics'),
+    ('E', 'Environment'),
+    ('M', 'Middle East'),
+    ('P', 'Palestine'),
+    ('L', 'Latin America'),
+    ('A', 'Africa'),
+    ('U', 'US Politics'),
+    ('S', 'Asia'),
+    ('X', 'Economics'),
+)
+
+class Article(models.Model):
+    headline = models.CharField(max_length=200)
+    link = models.CharField(max_length=200)
+    date = models.DateField()
+    description = models.TextField(max_length=250)
+    category = models.CharField(
+    max_length=1,
+    choices=CATEGORY,
+    default=CATEGORY[0][0]
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+import requests, json
+import environ 
+env = environ.Env()
+env.read_env()
+
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+def home(request):
+    response = requests.get(f'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key={env('NYT_KEY')}').json()
+    articles = response['response']['docs']  
+    return render(request, 'home.html', {'articles': articles})
