c6ef00fee324b361ffb2efc8fa0ed3e3fd9b2b72
Andrea Macias <andreamacrz@gmail.com>
2024-05-02 20:22:32 -0400

    added styling

--- infostream/settings.py
+++ infostream/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import environ
+import environ, os
 
 env = environ.Env()
 env.read_env()
@@ -123,7 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "main_app", "static"),
+]
 
 LOGIN_REDIRECT_URL = "/"
 
